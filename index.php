6e05217d1cc99a1a57aa036d41112b6b78184b25
Atma <152463084+bijoykumarsikdar@users.noreply.github.com>
2023-11-30 09:11:38 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,282 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link href="https://tubidymp3.world/favicon.ico" rel="icon" type="image/x-icon" />
+<title>Invisible Character – ( &#160; ) Blank Text Copy Paste</title>
+<meta name="description" content="Use our invisible character tool to generate an invisible letter to let you fill a form or send a message with blank input. Copy and paste the blank (empty) space for hiding names." />
+<link rel="canonical" href="https://tubidymp3.world/tool/invisible-character" />
+<link rel="alternate" hreflang="x-default" href="https://tubidymp3.world/tool/invisible-character/" />
+<link rel="alternate" hreflang="en" href="https://tubidymp3.world/tool/invisible-character/" />
+<link rel="alternate" hreflang="fr" href="https://tubidymp3.world/tool/invisible-character/" />
+<link rel="alternate" hreflang="es" href="https://tubidymp3.world/tool/invisible-character/" />
+<link rel="alternate" hreflang="id" href="https://tubidymp3.world/tool/invisible-character/" />
+<link rel="alternate" hreflang="br" href="https://tubidymp3.world/tool/invisible-character/" />
+<link rel="alternate" hreflang="de" href="https://tubidymp3.world/tool/invisible-character/" />
+<link rel="sitemap" type="application/xml" href="/sitemap.xml" title="TubidyMp3.World Site Map" />
+<style>
+    body{color:#000;font-family:verdana,"Comic Sans MS",Helvetica,sans-serif;margin:0;padding:0;font-size:small;line-height:20px}body *{margin:0}body,td{margin:0;padding:0;font-size:small}:link img,:visited img,a img{border:none}a{text-decoration:none;color:#124373;padding:5px}b{font-weight:700}input,select{padding:1px;margin:1px}textarea{font-size:small}.left{text-align:left}.center,.tCenter{text-align:center}.right{text-align:right}
+    .invisible-text {
+      color: rgba(0, 0, 0, 0);
+    }
+    .notepad {
+      border: 1px solid #ccc;
+      padding: 10px;
+      margin-top: 20px;
+      width: 90%;
+      height: 100px;
+      resize: vertical;
+    }
+    .logo{background:#22588E;background-image: linear-gradient(to top,#081623c4,#245586,#3979b9);color:#fff;padding:2px;text-align:center}.logo a{color:#fff;font-weight:700}
+    .footer{background:#22588E;background-image: linear-gradient(to top,#081623c4,#245586,#3979b9);margin-bottom:0;padding:10px;text-align:center;background-repeat:no-repeat;background-position:top left;border-bottom:3px solid #44a2ff;font-weight:700}.footer a{color:#fff}
+    
+.td:nth-child(4)::before {
+  content: attr(data-invisible-char); 
+  display: 
+}
+  </style>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7759139865135014" crossorigin="anonymous"></script>
+<meta name="google-adsense-account" content="ca-pub-7759139865135014">
+</head>
+<body>
+<header id="top">
+<div class="logo">
+<a href="https://tubidymp3.world/" title="TubidyMp3.World">
+<img width="250" height="50" alt="TubidyMp3.World" src="https://tubidymp3.world/images/download.svg" title="TubidyMp3.World" loading="lazy" /> </a>
+</div>
+</header>
+<style>.quick_links{width:100%;margin:0 auto}.quick_links a{background-image: linear-gradient(to top,#081623c4,#245586,#3979b9);color:#fefefe;padding:10px;display:inline-block;width:25%;vertical-align:middle;line-height:25px;border:1px solid #666;border-bottom:0;border-right:0;text-decoration:none;box-sizing:border-box}@media only screen and (max-width:500px){.quick_links a{width:50%}}.button{width:50px;padding:8px 25px;background: #323232;color: #fff !important;border-radius: 10px;margin:0 20px;} .button:hover{background: #888;}</style>
+<div class="quick_links"><b>
+<a href="/info/contact">Contact Us</a><a href="/info/disclaimer">Disclaimer</a><a href="/info/privacy">Privacy Policy</a><a href="/info/about">About Us
+</a>
+</b></div>
+</br>
+<h1>Invisible Character Tool [Text]</h1>
+<p>&#160; This is an example of an <span class="invisible-text">&#160;</span>invisible character.</p>
+</br>
+<button onclick="copyToClipboard()">Copy Invisible Character</button>
+</br>
+<textarea id="notepad" class="notepad" placeholder="Paste here to check the invisible character">&#160;</textarea>
+<script>
+function copyToClipboard() {
+  var textarea = document.getElementById('notepad');
+  
+  
+  textarea.select();
+  
+  try {
+    
+    var successful = document.execCommand('copy');
+    var message = successful ? 'Invisible character copied to clipboard!' : 'Unable to copy, please use Ctrl+C / Cmd+C to copy manually.';
+    alert(message);
+  } catch (err) {
+    alert('Error copying the invisible character. Please use Ctrl+C / Cmd+C to copy manually.');
+  }
+}
+</script>
+<div class="toc">
+<h2>Table of Contents</h2>
+<ul>
+<li><a href="#what-is-an-invisible-character">What is an Invisible Character?</a></li>
+<li><a href="#why-use-an-invisible-text-generator">Why Use an Invisible Text Generator?</a></li>
+<li><a href="#invisible-characters-where-to-use">Invisible Characters - Where to Use?</a>
+<ul>
+<li><a href="#invisible-characters-for-fortnite">Invisible Characters for Fortnite</a></li>
+<li><a href="#invisible-letters-for-instagram-twitter-and-pubg">Invisible Letters for Instagram, Twitter, and Pubg</a></li>
+<li><a href="#sending-an-empty-message-on-whatsapp">Sending an Empty Message on WhatsApp</a></li>
+</ul>
+</li>
+</ul>
+</div>
+<div class="flex-col" style="width:100%;margin-top:1rem">
+<div style="text-align: center;">
+<div class="content contentDiv" style="text-align: left;position:relative">
+<div class="dont-break-out" style="width: 100%;padding-right: 5px;">
+<h1 style="text-align:center">Invisible Character</h1>
+<p>You might want to copy and paste blank space.&nbsp;</p>
+<p>Invisible text generator lets you generate empty&nbsp;or blank text with one click.<br/>
+You can copy and paste the empty space for different purposes.</p>
+<h2>What is an Invisible Character?</h2>
+<p>Characters that appear as empty spaces, or invisible characters, are actually <b>Unicode characters</b></p>
+<p>They are normally used to represent a blank space without using the space key.<br/>
+The invisible letters are commonly used to send an empty message or set a form value to blank.</p>
+<p>In the Below mentioned table you can see [] brackets, each of them containing an invisible character</p>
+<table border="1">
+<thead>
+<tr>
+<th>Unicode</th>
+<th>Description</th>
+<th>Html</th>
+<th>Example</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>U+0020</td>
+<td>Space</td>
+<td>&amp;#32;</td>
+<td class="codeClick">[&#32;]</td>
+</tr>
+<tr>
+<td>U+00A0</td>
+<td>No-Break Space</td>
+<td>&amp;#160;</td>
+<td class="codeClick">[&#160;]</td>
+</tr>
+<tr>
+<td>U+2000</td>
+<td>En Quad</td>
+<td>&amp;#8192;</td>
+<td class="codeClick">[&#8192;]</td>
+</tr>
+<tr>
+<td>U+2001</td>
+<td>Em Quad</td>
+<td>&amp;#8193;</td>
+<td class="codeClick">[&#8193;]</td>
+</tr>
+<tr>
+<td>U+2002</td>
+<td>En Space</td>
+<td>&amp;#8194;</td>
+<td class="codeClick">[&#8194;]</td>
+</tr>
+<tr>
+<td>U+2003</td>
+<td>Em Space</td>
+<td>&amp;#8195;</td>
+<td class="codeClick">[&#8195;]</td>
+</tr>
+<tr>
+<td>U+2004</td>
+<td>Three-Per-Em Space</td>
+<td>&amp;#8196;</td>
+<td class="codeClick">[&#8196;]</td>
+</tr>
+<tr>
+<td>U+2005</td>
+<td>Four-Per-Em Space</td>
+<td>&amp;#8197;</td>
+<td class="codeClick">[&#8197;]</td>
+</tr>
+<tr>
+<td>U+2006</td>
+<td>Six-Per-Em Space</td>
+<td> &amp;#8198;</td>
+<td class="codeClick">[&#8198;]</td>
+</tr>
+<tr>
+<td>U+2007</td>
+<td>Figure Space</td>
+<td>&amp;#8199;</td>
+<td class="codeClick">[&#8199;]</td>
+</tr>
+<tr>
+<td>U+2008</td>
+<td>Punctuation Space</td>
+<td>&amp;#8200;</td>
+<td class="codeClick">[&#8200;]</td>
+</tr>
+<tr>
+<td>U+2009</td>
+<td>Thin Space</td>
+<td>&amp;#8201;</td>
+<td class="codeClick">[&#8201;]</td>
+</tr>
+<tr>
+<td>U+200A</td>
+<td>Hair Space</td>
+<td>&amp;#8202;</td>
+<td class="codeClick">[&#8202;]</td>
+</tr>
+<tr>
+<td>U+2028</td>
+<td>Line Separator</td>
+<td>&amp;#8232;</td>
+<td class="codeClick">[&#8232;]</td>
+</tr>
+<tr>
+<td>U+205F</td>
+<td>Medium Mathematical Space</td>
+<td>&amp;#8287;</td>
+<td class="codeClick">[&#8287;]</td>
+</tr>
+<tr>
+<td>U+3000</td>
+<td>Ideographic Space</td>
+<td>&amp;#12288;</td>
+<td class="codeClick">[&#12288;]</td>
+</tr>
+</tbody>
+</table>
+<h2>Why use an invisible text generator?</h2>
+<p>With this tool, you can easily send an empty message, either on a website or a mobile application.</p>
+<p>Simply copy the blank space&nbsp;by clicking <em><strong>Copy to Clipboard&nbsp;</strong></em>button and paste invisible text or blank characters.</p>
+<h3>No Installation</h3>
+<p>There is no need to download and install any software to generate empty letters.&nbsp;<br/>
+Our tool requires no installation or signup process to generate invisible characters.</p>
+<h3>Copy-Paste Empty Text</h3>
+<p>This feature helps you to send or show empty characters wherever your browser does not allow it.<br/>
+It generates invisible text and allows you to copy and paste it anywhere without any hurdle.</p>
+<h3>Testing Option</h3>
+<p>Once you copy the text to the clipboard, this option allows you to test the generated empty space.</p>
+<h3>Free to Use</h3>
+<p>The tool is completely free to use for all online purposes. You can either use it for sending blank spaces on social media apps or other online platforms.</p>
+<h2>Invisible Characters - Where to Use?</h2>
+<p>You can use the invisible font for several purposes including:</p>
+<h3>Invisible Characters for Fortnite</h3>
+<p>The invisible symbol can be used in Fortnite RPG to hide usernames. You can easily generate these empty letters with this tool.&nbsp;</p>
+<h3>Invisible Letters for Instagram, Twitter, and Pubg</h3>
+<p>The empty spaces can also be used on different social media platforms.&nbsp;</p>
+<p>These characters are used to create usernames for your social media accounts on Twitter and Instagram.&nbsp;</p>
+<p>You can also use the invisible words to create a unique username for gaming i.e., PUBG or other mobile apps.</p>
+<p>You can also use our <u>fake address generator</u> to create fake social media and gaming profiles to hide your identity.</p>
+<h3>Sending an Empty Message on WhatsApp</h3>
+<p>Online texting apps like WhatsApp do not allow you to send a blank message even if you type empty spaces. You can generate a blank space by using our invisible text generator tool and send a blank text on WhatsApp.</p>
+<p>You can also&nbsp;use it to send an empty message on your mobile&#39;s default messaging app.<br/>
+Note: This &ldquo;Blank space copy and paste&rdquo; is not a real space, it is a character.</p>
+<h2>Why Use an Invisible Text Generator?</h2>
+<p>With this tool, you can easily send an empty message, either on a website or a mobile application. Simply copy the blank space by clicking <em><strong>Copy to Clipboard</strong></em> button and paste invisible text or blank characters.</p>
+
+<h3>No Installation</h3>
+<p>There is no need to download and install any software to generate empty letters. Our tool requires no installation or signup process to generate invisible characters.</p>
+<h3>Copy-Paste Empty Text</h3>
+<p>This feature helps you to send or show empty characters wherever your browser does not allow it. It generates invisible text and allows you to copy and paste it anywhere without any hurdle.</p>
+<h3>Testing Option</h3>
+<p>Once you copy the text to the clipboard, this option allows you to test the generated empty space.</p>
+<h3>Free to Use</h3>
+<p>The tool is completely free to use for all online purposes. You can either use it for sending blank spaces on social media apps or other online platforms.</p>
+<h2>Invisible Characters - Where to Use?</h2>
+<p>You can use the invisible font for several purposes including:</p>
+<h3>Invisible Characters for Fortnite</h3>
+<p>The invisible symbol can be used in Fortnite RPG to hide usernames. You can easily generate these empty letters with this tool.</p>
+<h3>Invisible Letters for Instagram, Twitter, and Pubg</h3>
+<p>The empty spaces can also be used on different social media platforms. These characters are used to create usernames for your social media accounts on Twitter and Instagram. You can also use the invisible words to create a unique username for gaming i.e., PUBG or other mobile apps.</p>
+<p>You can also use our <u>fake address generator</u> to create fake social media and gaming profiles to hide your identity.</p>
+<h3>Sending an Empty Message on WhatsApp</h3>
+<p>Online texting apps like WhatsApp do not allow you to send a blank message even if you type empty spaces. You can generate a blank space by using our invisible text generator tool and send a blank text on WhatsApp. You can also use it to send an empty message on your mobile's default messaging app. Note: This "Blank space copy and paste" is not a real space, it is a character.</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<footer>
+<div class="footer white">©
+2023 TubidyMp3.World&trade;
+<div class="tCenter">
+<a title="Contact" href="https://tubidymp3.world/info/contact"><b>Contact</b></a>|<a title="Disclaimer" href="https://tubidymp3.world/info/disclaimer"><b>Disclaimer</b></a>|<a title="Privacy Policy" href="https://tubidymp3.world/info/privacy"><b>Privacy Policy</b></a>|<a title="About" href="https://tubidymp3.world/info/about"><b>About</b></a> </div>
+</div>
+</footer>
+</body>
+</html>
